a0acdbecf6d7dc715c8c951c538880856fd622f7
JakubSyperek <162420@student.uwm.edu.pl>
2021-03-07 20:37:12 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+lista = ['Chlopaki nie placza', 'Kiler', 'Pulp Fiction', 'Poranek kojota', 'Nie lubie poniedzialku', 'Ratatuj']
+print(lista)
+
+lista.reverse()
+
+print('Odwrócona lista: ', lista)
+
+lista.insert(5, 'Podroz na ksiezyc')
+lista.insert(5,'Gulczas, a jak myslisz?')
+lista.insert(5, 'Dzien swira')
+lista.insert(5, 'Shrek 2')
+
+print('Nowa lista: ', lista)
